3b328eec5ee0ae5a7061377f55a41d42a788b604
alt-linux <amogyous@gmail.com>
2026-02-26 19:55:21 +0300

    Add files via upload

--- m3-br-srv-6-2.sh
+++ m3-br-srv-6-2.sh
@@ -1,3 +1,1 @@
-rm -rf /etc/rsyslog.d/00_common.conf
-cp /down/00_common.conf1 /etc/rsyslog.d/00_common.conf
-systemctl enable --now rsyslog
+echo "rm -rf /etc/rsyslog.d/00_common.conf && cp /down/00_common.conf1 /etc/rsyslog.d/00_common.conf && systemctl enable --now rsyslog"
